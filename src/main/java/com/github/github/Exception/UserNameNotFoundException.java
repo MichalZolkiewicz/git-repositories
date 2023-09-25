2057fb16b57fac3cfcb3e873a4b1736e0ad4a781
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-09-26 01:49:58 +0200

    constructor added

--- src/main/java/com/github/github/Exception/UserNameNotFoundException.java
+++ src/main/java/com/github/github/Exception/UserNameNotFoundException.java
@@ -1,9 +1,17 @@
 package com.github.github.Exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Username not found")
 public class UserNameNotFoundException extends Exception {
 
+    private final HttpStatusCode status;
+    private final String message;
+
+    public UserNameNotFoundException(HttpStatusCode status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
